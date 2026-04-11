f8662ee981102b78049ae860223e320c53656e3d
galileo <galileo@genesisISEEYOUstartthehunt.local>
2026-04-11 15:04:30 +0200

    temporary homepage background image tester

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,25 @@ import Testimonials from "@/components/Testimonials";
 import CallToAction from "@/components/CallToAction";
 import Footer from "@/components/Footer";
 import SmoothScroll from "@/components/SmoothScroll";
+import BgTester from "@/components/BgTester";
 
 export default function Home() {
   return (
     <SmoothScroll>
-      <Navigation />
-      <main>
-        <Hero />
-        <Services />
-        <Work />
-        <Process />
-        <FAQ />
-        <Testimonials />
-        <CallToAction />
-      </main>
-      <Footer />
+      <BgTester />
+      <div className="relative z-10">
+        <Navigation />
+        <main>
+          <Hero />
+          <Services />
+          <Work />
+          <Process />
+          <FAQ />
+          <Testimonials />
+          <CallToAction />
+        </main>
+        <Footer />
+      </div>
     </SmoothScroll>
   );
 }
